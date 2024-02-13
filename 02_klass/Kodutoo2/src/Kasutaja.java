403a06f91ecdce0eb79e2264d7413940ce319e7f
Jörgen Kristofer Rebane <jkrebane@gmail.com>
2024-02-13 15:36:10 +0200

    Kodutoo example

---
+++ 02_klass/Kodutoo2/src/Kasutaja.java
@@ -0,0 +1,25 @@
+public class Kasutaja {
+    String nimi; // sona
+    boolean aktiivne; // truefalse
+    int vanus; // taisarv
+    char lyhend; // yks taht
+    double raha; // komakohaga arv
+
+    // parem klops -> generate -> constructor
+    public Kasutaja(String nimi, boolean aktiivne, int vanus, char lyhend, double raha) {
+        this.nimi = nimi;
+        this.aktiivne = aktiivne;
+        this.vanus = vanus;
+        this.lyhend = lyhend;
+        this.raha = raha;
+    }
+
+    public void muudaAktiivne() {
+        aktiivne = !aktiivne;
+    }
+
+    public void lisaRaha(double summa) {
+        raha = raha + summa;
+    }
+
+}
--- 02_klass/Kodutoo2/src/Main.java
+++ 02_klass/Kodutoo2/src/Main.java
@@ -1,15 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Main class + enda tehtud class
+        // Classis constructor
+        // Vahemalt 2 omadust
+        // Vahemalt 2 funktsiooni
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Mainis vahemalt 2 instantsi temast
+        // Mainis vahemalt 2 funktsiooni valjakutset
+        // Vahemalt 2 System.out.println()
+
+        Kasutaja kasutaja = new Kasutaja("M", true, 20, 'x', 0.0);
+        Kasutaja kasutaja2 = new Kasutaja("S", false, 33, 'd', 499.0);
+
+        kasutaja.muudaAktiivne();
+        System.out.println(kasutaja);
+        double kasutaja2Raha = kasutaja2.lisaRaha(500.0);
+        System.out.println(kasutaja2Raha);
+
+        Tellimus tellimus1 = new Tellimus("M",199.0,"Coca");
+        Tellimus tellimus2 = new Tellimus("S",211.0,"Fanta");
+
+        double dollarites = tellimus1.tellimuseSummaDollarites();
+        System.out.println(dollarites);
+
+        tellimus2.tellijaNimeVahetus("Y");
+        System.out.println(tellimus2.tellija);
+
     }
 }
