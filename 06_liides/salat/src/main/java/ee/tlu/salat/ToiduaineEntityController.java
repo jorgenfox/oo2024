c105783507bbefb7b30b7462efe6ea68ec81781c
Jörgen Kristofer Rebane <jkrebane@gmail.com>
2024-03-12 15:18:49 +0200

    Tund6 loppenud

---
+++ 06_liides/salat/src/main/java/ee/tlu/salat/ToiduaineEntityController.java
@@ -0,0 +1,63 @@
+package ee.tlu.salat;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class ToiduaineEntityController {
+    List<ToiduaineEntity> toiduained = new ArrayList<>(); // imiteerime andmebaasi
+
+    // localhost:8080/api/toiduained
+    @GetMapping("toiduained")
+    public List<ToiduaineEntity> saaToiduained() {
+        return toiduained;
+    }
+
+    // localhost:8080/api/toiduained/Kartul/10/5/5
+    // ON jarjekord tahtis
+    // ma voin kogemata sassi ajada, muutes jarjekorda ja paneb valesti
+    // ma ei saa uhtegi vahele jatta
+    @PostMapping("toiduained/{nimi}/{valk}/{rasv}/{sysivesik}")
+    public List<ToiduaineEntity> lisaToiduaine(
+            @PathVariable String nimi,
+            @PathVariable int valk,
+            @PathVariable int rasv,
+            @PathVariable int sysivesik
+    ) {
+        if (valk + rasv + sysivesik > 100) {
+            return toiduained;
+        }
+        ToiduaineEntity toiduaine = new ToiduaineEntity(nimi, valk, rasv, sysivesik);
+        toiduained.add(toiduaine);
+        return toiduained;
+    }
+
+    @DeleteMapping("toiduained/{index}")
+    public List<ToiduaineEntity> kustutaToiduaine(@PathVariable int index) {
+        toiduained.remove(index);
+        return toiduained;
+    }
+
+    // localhost:8080/api/toiduained?index=0&nimi=Kartul&valk=10&rasv=5&sysivesik=5
+    // lubab mingil moel vahele jatta mingid vaartused
+    @PutMapping("toiduained")
+    public List<ToiduaineEntity> muudaToiduaine(
+            @RequestParam int index,
+            @RequestParam String nimi,
+            @RequestParam int valk,
+            @RequestParam int rasv,
+            @RequestParam int sysivesik
+    ) {
+        ToiduaineEntity toiduaine = new ToiduaineEntity(nimi, valk, rasv, sysivesik);
+        toiduained.set(index, toiduaine);
+        return toiduained;
+    }
+
+    @GetMapping("toiduained/{index}")
+    public ToiduaineEntity saaYksToiduaine(@PathVariable int index) {
+        return toiduained.get(index);
+    }
+}
